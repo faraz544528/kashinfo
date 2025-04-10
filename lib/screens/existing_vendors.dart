ec9212d70eca25b600bb220fe1ce8f1d587b0e56
faraz544528 <159261890+faraz544528@users.noreply.github.com>
2025-04-10 14:42:18 +0530

    API calls

--- lib/screens/existing_vendors.dart
+++ lib/screens/existing_vendors.dart
@@ -1,5 +1,10 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
 import 'package:kashinfo/data/controllers.dart';
+import 'package:kashinfo/widgets/my_buttons.dart';
+import 'package:kashinfo/widgets/my_text_field.dart';
 
 class ExistingVendors extends StatefulWidget {
   final List<Map<String, String>> vendors;
@@ -10,20 +15,66 @@ class ExistingVendors extends StatefulWidget {
 }
 
 class _ExistingVendorsState extends State<ExistingVendors> {
+  String? firstName;
+  String? lastName;
+  String? image;
+  Future<void> fetchDetails() async {
+    var data = await http.get(Uri.parse("https://reqres.in/api/users"));
+    var decoder = jsonDecode(data.body);
+    setState(() {
+      firstName = "FirstName: ${decoder["data"][0]["first_name"]}";
+      lastName = "LastName: ${decoder["data"][0]["last_name"]}";
+      image = decoder["data"][0]["avatar"];
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(title: Text("Existing vendors")),
-        body: ListView.builder(
-          itemCount: widget.vendors.length,
-          itemBuilder: (context, index) {
-            final vendor = widget.vendors[index];
-            return ListTile(
-              leading: Image(image: NetworkImage(vendor['Image URL'] ?? "")),
-              title: Text(vendor['Vendor Name'] ?? ""),
-              subtitle: Text(vendor['Email'] ?? ""),
-            );
-          },
-        ));
+      appBar: AppBar(title: Text("Existing vendors")),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            CustomButton(
+              onPressed: fetchDetails,
+              text: "HIT API",
+            ),
+            if (firstName != null && lastName != null && image != null)
+              Container(
+                height: 100,
+                width: 200,
+                color: Colors.grey,
+                child: Row(
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.only(left: 15),
+                      child: CircleAvatar(
+                        radius: 20,
+                        backgroundImage: NetworkImage(image!),
+                      ),
+                    ),
+                    SizedBox(
+                      width: 10,
+                    ),
+                    Text("$firstName \n$lastName"),
+                  ],
+                ),
+              )
+          ],
+        ),
+      ),
+
+      // body: ListView.builder(
+      //   itemCount: widget.vendors.length,
+      //   itemBuilder: (context, index) {
+      //     final vendor = widget.vendors[index];
+      //     return ListTile(
+      //       leading: Image(image: NetworkImage(vendor['Image URL'] ?? "")),
+      //       title: Text(vendor['Vendor Name'] ?? ""),
+      //       subtitle: Text(vendor['Email'] ?? ""),
+      //     );
+      //  },
+    );
   }
 }
