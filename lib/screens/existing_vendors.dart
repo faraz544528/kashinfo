2e4353c97c5710b5f89caf5eb6605baaaac176dd
faraz544528 <159261890+faraz544528@users.noreply.github.com>
2025-04-17 23:40:20 +0530

    inIt State

--- lib/screens/existing_vendors.dart
+++ lib/screens/existing_vendors.dart
@@ -1,3 +1,4 @@
+import 'dart:async';
 import 'dart:convert';
 
 import 'package:flutter/material.dart';
@@ -9,7 +10,7 @@ import 'package:kashinfo/widgets/my_text_field.dart';
 
 class ExistingVendors extends StatefulWidget {
   final List<Map<String, String>> vendors;
-  const ExistingVendors({super.key, required this.vendors});
+  ExistingVendors({super.key, required this.vendors});
 
   @override
   State<ExistingVendors> createState() => _ExistingVendorsState();
@@ -19,76 +20,72 @@ class _ExistingVendorsState extends State<ExistingVendors> {
   String? firstName;
   String? lastName;
   String? image;
+  List<Map<String, dynamic>> push = [];
   Future<void> fetchDetails() async {
     var data = await http.get(Uri.parse("https://reqres.in/api/users"));
     var decoder = jsonDecode(data.body);
+
     setState(() {
-      firstName = "FirstName: ${decoder["data"][0]["first_name"]}";
-      lastName = "LastName: ${decoder["data"][0]["last_name"]}";
-      image = decoder["data"][0]["avatar"];
+      decoder["data"].forEach((element) {
+        push.add(element);
+        // print(push);
+      });
+
+      // firstName = "FirstName: ${decoder["data"][0]["first_name"]}";
+      // lastName = "LastName: ${decoder["data"][0]["last_name"]}";
+      // image = ["data"][0]["avatar"];
     });
   }
 
+  void initState() {
+    super.initState();
+    fetchDetails();
+  }
+
   @override
-  Widget build(BuildContext context) {
+  Widget build(
+    BuildContext context,
+  ) {
     return Scaffold(
       appBar: AppBar(title: Text("Existing vendors")),
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            CustomButton(
-              onPressed: fetchDetails,
-              text: "HIT API",
-            ),
-            SizedBox(
-              height: 30,
-            ),
-            if (firstName != null && lastName != null && image != null)
+            if (push.length > 0)
               Card(
-                elevation: 2,
-                child: Container(
-                  height: 100,
-                  width: 200,
-                  decoration: BoxDecoration(
-                    boxShadow: [
-                      BoxShadow(color: Colors.red, blurStyle: BlurStyle.outer)
-                    ],
-                    borderRadius: BorderRadius.circular(16),
-                    gradient: LinearGradient(
-                        colors: [AppColors.orange, AppColors.pink]),
-                  ),
-                  child: Row(
-                    children: [
-                      Padding(
-                        padding: const EdgeInsets.only(left: 15),
-                        child: CircleAvatar(
-                          radius: 20,
-                          backgroundImage: NetworkImage(image!),
-                        ),
-                      ),
-                      SizedBox(
-                        width: 10,
-                      ),
-                      Text("$firstName \n$lastName"),
-                    ],
-                  ),
-                ),
-              )
+                  elevation: 4,
+                  child: SizedBox(
+                    height: 300,
+                    width: 500,
+                    child: ListView.builder(
+                        itemCount: push.length,
+                        itemBuilder: (context, index) {
+                          final pushValue = push[index];
+                          return Padding(
+                            padding: const EdgeInsets.all(10.0),
+                            child: Container(
+                                height: 100,
+                                width: 400,
+                                decoration: BoxDecoration(
+                                    borderRadius: BorderRadius.circular(16),
+                                    gradient: LinearGradient(colors: [
+                                      AppColors.orange,
+                                      AppColors.pink
+                                    ])),
+                                child: ListTile(
+                                  leading: Image(
+                                      image: NetworkImage(
+                                          pushValue['avatar'] ?? "")),
+                                  title: Text(pushValue['first_name'] ?? ""),
+                                  subtitle: Text(pushValue['email'] ?? ""),
+                                )),
+                          );
+                        }),
+                  )),
           ],
         ),
       ),
-
-      // body: ListView.builder(
-      //   itemCount: widget.vendors.length,
-      //   itemBuilder: (context, index) {
-      //     final vendor = widget.vendors[index];
-      //     return ListTile(
-      //       leading: Image(image: NetworkImage(vendor['Image URL'] ?? "")),
-      //       title: Text(vendor['Vendor Name'] ?? ""),
-      //       subtitle: Text(vendor['Email'] ?? ""),
-      //     );
-      //  },
     );
   }
 }
